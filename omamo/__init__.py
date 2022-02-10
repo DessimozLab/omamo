62ce68011cd29c5df491ac7aaf3a770df4d47c34
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2022-02-10 11:11:11 +0100

    Bump version: 0.2.0 → 0.2.1

--- omamo/__init__.py
+++ omamo/__init__.py
@@ -2,4 +2,4 @@ from .ic import load_informationcontent_from_file
 from .inference import build_omamo
 
 __all__ = [load_informationcontent_from_file, build_omamo]
-__version__ = "0.2.0"
+__version__ = "0.2.1"
